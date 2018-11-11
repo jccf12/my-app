5a9d778db11e2933139f4ed3599d5bd1abc015ad
Juan Castro <jccf.12@hotmail.com>
2018-11-12 03:16:01 +0900

    Updates

---
+++ precache-manifest.9492594ddeb8fa24444abf2ac3107e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d3e0344860ed9cf5bdf",
+    "url": "/my-app/static/css/main.fa117355.chunk.css"
+  },
+  {
+    "revision": "8d3e0344860ed9cf5bdf",
+    "url": "/my-app/static/js/main.8d3e0344.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/my-app/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6bcb4636321851cb6efaf3631e2cf9d7",
+    "url": "/my-app/index.html"
+  }
+];
